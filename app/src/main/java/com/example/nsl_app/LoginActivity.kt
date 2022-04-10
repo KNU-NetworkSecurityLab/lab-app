b4e0d55269960c1d639d901680a18f2e559e8592
uknow <yoon6763@naver.com>
2022-04-10 12:21:22 +0900

    기존 MainActivity -> LoginActivity 이름 변경

--- app/src/main/java/com/example/nsl_app/LoginActivity.kt
+++ app/src/main/java/com/example/nsl_app/LoginActivity.kt
@@ -1,19 +1,21 @@
 package com.example.nsl_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.nsl_app.databinding.ActivityMainBinding
+import com.example.nsl_app.databinding.ActivityLoginBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btnLogin.setOnClickListener {
-
+            val intent = Intent(applicationContext, MainBaseActivity::class.java)
+            startActivity(intent)
         }
     }
 }
