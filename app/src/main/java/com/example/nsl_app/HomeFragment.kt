fc9be50a328df52f321e2f9e7c1bb7dc0ab815a2
uknow <yoon6763@naver.com>
2022-04-17 23:50:42 +0900

    홈페이지 화면 레이아웃 추가

--- app/src/main/java/com/example/nsl_app/HomeFragment.kt
+++ app/src/main/java/com/example/nsl_app/HomeFragment.kt
@@ -1,12 +1,16 @@
 package com.example.nsl_app
 
+import android.graphics.Point
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import com.example.nsl_app.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +21,36 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        val display = activity?.windowManager?.defaultDisplay
+        val size = Point()
+        display?.getRealSize(size)
+
+        val deviceWidth = size.x
+        val containerSize = (deviceWidth * 0.35).toInt()
+
+        binding.run {
+            containerSmartDoor.layoutParams.run {
+                height = containerSize
+                width = containerSize
+            }
+            containerSmartCoffee.layoutParams.run {
+                height = containerSize
+                width = containerSize
+            }
+            containerSmartLight.layoutParams.run {
+                height = containerSize
+                width = containerSize
+            }
+            containerSmartPaper.layoutParams.run {
+                height = containerSize
+                width = containerSize
+            }
+        }
+
+        return binding.root
     }
+
+
 }
