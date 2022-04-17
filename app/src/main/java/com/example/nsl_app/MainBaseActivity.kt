c1129e8fc10cb4164d1e1e0342d3ef2913be96b0
uknow <yoon6763@naver.com>
2022-04-17 21:59:21 +0900

    BottomNavigationbar 추가

--- app/src/main/java/com/example/nsl_app/MainBaseActivity.kt
+++ app/src/main/java/com/example/nsl_app/MainBaseActivity.kt
@@ -2,12 +2,48 @@ package com.example.nsl_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.example.nsl_app.databinding.ActivityMainBaseBinding
+import com.google.android.material.navigation.NavigationBarView
+import java.util.zip.Inflater
 
 class MainBaseActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBaseBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_base)
+        binding = ActivityMainBaseBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+
+        binding.run {
+
+            supportFragmentManager.beginTransaction().add(R.id.container_main,HomeFragment()).commit()
+            navMain.setOnItemSelectedListener {
+                when(it.itemId) {
+                    R.id.nav_home -> {
+                        supportFragmentManager.beginTransaction().replace(R.id.container_main, HomeFragment()).commit()
+                        return@setOnItemSelectedListener true
+                    }
+
+                    R.id.nav_community -> {
+                        supportFragmentManager.beginTransaction().replace(R.id.container_main, CommunityFragment()).commit()
+                        return@setOnItemSelectedListener true
+                    }
+
+                    R.id.nav_schedule -> {
+                        supportFragmentManager.beginTransaction().replace(R.id.container_main, ScheduleFragment()).commit()
+                        return@setOnItemSelectedListener true
+                    }
+
+                    R.id.nav_my_page -> {
+                        supportFragmentManager.beginTransaction().replace(R.id.container_main, MyPageFragment()).commit()
+                        return@setOnItemSelectedListener true
+                    }
+                }
+                return@setOnItemSelectedListener false
+            }
+        }
 
     }
 }
