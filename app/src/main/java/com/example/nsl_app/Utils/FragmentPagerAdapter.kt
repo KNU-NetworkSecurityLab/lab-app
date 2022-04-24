5393e240a054a087c87d5625cdee77fef9319253
uknow <yoon6763@naver.com>
2022-04-24 10:21:40 +0900

    프래그먼트 페이저 어댑터 추가

---
+++ app/src/main/java/com/example/nsl_app/Utils/FragmentPagerAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.nsl_app.Utils
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class FragmentPagerAdapter(fragmentActivity: FragmentActivity): FragmentStateAdapter(fragmentActivity) {
+    var fragmentList = ArrayList<Fragment>()
+
+    override fun getItemCount(): Int {
+        return fragmentList.size
+    }
+
+    fun addFragment(fragment:Fragment) {
+        fragmentList.add(fragment)
+        notifyItemInserted(fragmentList.size -1)
+    }
+
+    fun removeFragment() {
+        fragmentList.removeLast()
+        notifyItemInserted(fragmentList.size)
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragmentList[position]
+    }
+}
