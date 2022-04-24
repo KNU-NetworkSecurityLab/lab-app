2108d95e3d25682d6d9f615a9f0e3f3a7e7c470f
uknow <yoon6763@naver.com>
2022-04-24 10:22:23 +0900

    뷰 페이저(프래그먼트 페이저) 적용

--- app/src/main/java/com/example/nsl_app/ScheduleFragment.kt
+++ app/src/main/java/com/example/nsl_app/ScheduleFragment.kt
@@ -1,12 +1,17 @@
 package com.example.nsl_app
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
+import com.example.nsl_app.Utils.FragmentPagerAdapter
+import com.example.nsl_app.databinding.FragmentScheduleBinding
 
 class ScheduleFragment : Fragment() {
+    private lateinit var binding: FragmentScheduleBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +22,29 @@ class ScheduleFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schedule, container, false)
+
+        binding = FragmentScheduleBinding.inflate(inflater, container, false)
+
+        val fragmentPagerAdapter = FragmentPagerAdapter(requireActivity())
+        fragmentPagerAdapter.addFragment(ScheduleCalendarFragment())
+        fragmentPagerAdapter.addFragment(ScheduleMeetingFragment())
+
+
+        binding.run {
+            viewPagerSchedule.adapter = fragmentPagerAdapter
+            viewPagerSchedule.registerOnPageChangeCallback(object :
+                ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+
+                }
+            })
+        }
+
+
+
+        return binding.root
     }
+
+
 }
