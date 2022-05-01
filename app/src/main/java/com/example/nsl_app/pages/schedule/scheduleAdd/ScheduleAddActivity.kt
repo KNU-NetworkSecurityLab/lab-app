17d01f65ab50f685b7e6f570eb652f0a6a0f03ee
uknow <yoon6763@naver.com>
2022-05-01 22:25:35 +0900

    Color, Dimen, String, Drawable 추가 및 업데이트

--- app/src/main/java/com/example/nsl_app/pages/schedule/scheduleAdd/ScheduleAddActivity.kt
+++ app/src/main/java/com/example/nsl_app/pages/schedule/scheduleAdd/ScheduleAddActivity.kt
@@ -3,10 +3,21 @@ package com.example.nsl_app.pages.schedule
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
 import com.example.nsl_app.databinding.ActivityScheduleAddBinding
+import com.example.nsl_app.utils.TagClickListener
 
 class ScheduleAddActivity : AppCompatActivity() {
     private lateinit var binding: ActivityScheduleAddBinding
+
+
+
+
+    private val tagClickListener = object : TagClickListener {
+        override fun onTagClick(tag: String) {
+            Toast.makeText(applicationContext,"$tag 선택됨",Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +32,14 @@ class ScheduleAddActivity : AppCompatActivity() {
         }
 
 
+
+        binding.run {
+            tvCalTagAdd.setOnClickListener {
+                val scheduleAddTagDialogFragment = ScheduleAddTagDialogFragment().newInstance(tagClickListener)
+
+                scheduleAddTagDialogFragment.show(supportFragmentManager,"NewCalendarTagAdd")
+            }
+        }
     }
 
 
