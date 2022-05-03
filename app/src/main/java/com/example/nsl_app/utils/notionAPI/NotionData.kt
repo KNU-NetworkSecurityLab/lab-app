3e340d0d222b9d0bc6202ed56057efe427e6d257
uknow <yoon6763@naver.com>
2022-05-03 23:46:08 +0900

    NotionCreateScheduleWithContentData DTO 추가

--- app/src/main/java/com/example/nsl_app/utils/notionAPI/NotionData.kt
+++ app/src/main/java/com/example/nsl_app/utils/notionAPI/NotionData.kt
@@ -1,9 +1,16 @@
 package com.example.nsl_app.utils.notionAPI
 
-import com.example.nsl_app.utils.notionAPI.dataDTO.Parent
-import com.example.nsl_app.utils.notionAPI.dataDTO.Properties
+import com.example.nsl_app.utils.notionAPI.dataDTO.registerScheduleWithContent.Children
+import com.example.nsl_app.utils.notionAPI.dataDTO.registerScheduleWithContent.Parent
+import com.example.nsl_app.utils.notionAPI.dataDTO.registerScheduleWithContent.Properties
 
 data class NotionCreateScheduleData(
     val parent: Parent,
     val properties: Properties
+)
+
+data class NotionCreateScheduleWithContentData(
+    val parent: Parent,
+    val properties: Properties,
+    val children: List<Children>
 )
