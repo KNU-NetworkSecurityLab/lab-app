e16731a77c39d5f73acaa1ec703f87370febf7e8
uknow <yoon6763@naver.com>
2022-05-14 19:32:54 +0900

    일정 리스트뷰에 시간 표시

--- app/src/main/java/com/example/nsl_app/pages/schedule/scheduleList/ScheduleAdapter.kt
+++ app/src/main/java/com/example/nsl_app/pages/schedule/scheduleList/ScheduleAdapter.kt
@@ -8,9 +8,13 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.nsl_app.R
 import com.example.nsl_app.pages.schedule.ScheduleFragment
+import java.text.SimpleDateFormat
 
 class ScheduleAdapter (val context: Context, private val scheduleItemList: ArrayList<ScheduleFragment.ScheduleData>) :
     RecyclerView.Adapter<ScheduleAdapter.Holder>() {
+
+    val dateFormat = SimpleDateFormat("M월 d일")
+    val dateTimeFormat = SimpleDateFormat("M월 d일\na h:mm")
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
@@ -21,7 +25,28 @@ class ScheduleAdapter (val context: Context, private val scheduleItemList: Array
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
         holder.run {
-            tv_item_date_title.text = scheduleItemList[position].title
+
+            val scheduleItem = scheduleItemList[position]
+
+            tv_item_date_title.text = scheduleItem.title
+
+            tv_item_date_start.text = if(scheduleItem.startIsIncludeTime) {
+                dateTimeFormat.format(scheduleItem.startDate!!.time)
+            } else {
+                dateFormat.format(scheduleItem.startDate!!.time)
+            }
+
+            if(scheduleItem.endDate == null) {
+                tv_item_date_end.visibility = View.GONE
+            } else {
+                tv_item_date_end.visibility = View.VISIBLE
+
+                if(scheduleItem.endIsIncludeTime) {
+                    tv_item_date_end.text = dateTimeFormat.format(scheduleItem.endDate!!.time)
+                } else {
+                    tv_item_date_end.text = dateFormat.format(scheduleItem.endDate!!.time)
+                }
+            }
         }
     }
 
