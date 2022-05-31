379a7139917534ba4c2c7102cc74e0f7adef0f79
uknow <yoon6763@naver.com>
2022-05-31 20:56:58 +0900

    테마 일괄 수정

--- app/src/main/java/com/example/nsl_app/pages/loginAndRegister/RegisterActivity.kt
+++ app/src/main/java/com/example/nsl_app/pages/loginAndRegister/RegisterActivity.kt
@@ -2,6 +2,7 @@ package com.example.nsl_app.pages.loginAndRegister
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.example.nsl_app.R
 import com.example.nsl_app.databinding.ActivityRegisterBinding
 
@@ -12,7 +13,26 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.run {
+            setSupportActionBar(binding.toolbarRegister)
+            supportActionBar?.run {
+                // 앱 바 뒤로가기 버튼 설정
+                setDisplayHomeAsUpEnabled(true)
+            }
+
+
             btnRegFinish.setOnClickListener { finish() }
         }
+    }
+
+    
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // 앱 바 클릭 이벤트
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
