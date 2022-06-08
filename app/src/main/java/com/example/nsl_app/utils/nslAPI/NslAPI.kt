a4b9db974d52d2180a7a78b7523368152db3ff8b
전윤호@랩실컴 <yoon6763@naver.com>
2022-06-08 15:39:45 +0900

    NSL API 로그인 & 회원가입 추가

---
+++ app/src/main/java/com/example/nsl_app/utils/nslAPI/NslAPI.kt
@@ -0,0 +1,36 @@
+package com.example.nsl_app.utils.nslAPI
+
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.*
+
+interface NslAPI {
+
+    companion object {
+        private const val baseUrl = NSlUrl.NSL_URL
+
+        fun create(): NslAPI {
+            return Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(NslAPI::class.java)
+        }
+    }
+
+    /*
+        User
+     */
+
+    // 로그인
+    @POST("login")
+    fun loginCall(@Body body: LoginRequestDTO): Call<ResponseBody>
+
+    // 회원가입
+    @POST("api/v1/signup")
+    fun signUpCall(@Body body: SignUpRequestDTO): Call<ResponseBody>
+
+
+}
---
+++ app/src/main/java/com/example/nsl_app/utils/nslAPI/NslRequestDTO.kt
@@ -0,0 +1,16 @@
+package com.example.nsl_app.utils.nslAPI
+
+import com.google.gson.annotations.SerializedName
+
+data class LoginRequestDTO(
+    @SerializedName("userId") val userId: String,
+    @SerializedName("password") val password: String
+)
+
+data class SignUpRequestDTO(
+    @SerializedName("userId") val userId: String,
+    @SerializedName("name") val name: String,
+    @SerializedName("password") val password: String,
+    @SerializedName("mail") val mail: String,
+    @SerializedName("phone") val phone: String
+)
