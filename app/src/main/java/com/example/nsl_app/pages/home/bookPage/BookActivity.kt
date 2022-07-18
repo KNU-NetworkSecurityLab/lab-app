d10170f44ff7305e10ca983c20b1518c3d96d8a8
전윤호@랩실컴 <yoon6763@naver.com>
2022-07-18 14:11:11 +0900

    북 페이지 추가

--- app/src/main/java/com/example/nsl_app/pages/home/bookPage/BookActivity.kt
+++ app/src/main/java/com/example/nsl_app/pages/home/bookPage/BookActivity.kt
@@ -1,11 +1,13 @@
 package com.example.nsl_app.pages.home.bookPage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.nsl_app.databinding.ActivityBookBinding
+import com.example.nsl_app.pages.home.bookPage.bookAdd.BookAddActivity
 
 class BookActivity : AppCompatActivity() {
     private val binding by lazy { ActivityBookBinding.inflate(layoutInflater) }
@@ -26,16 +28,21 @@ class BookActivity : AppCompatActivity() {
         tempTags.add("Java")
         tempTags.add("안드로이드")
 
-        bookList.add(BookItem("Do it! 안드로이드","강XX",tempTags))
-        bookList.add(BookItem("월급 두배로 받는 법","전XX",tempTags))
-        bookList.add(BookItem("학점 All A+ 받는 법","성XX",tempTags))
+        bookList.add(BookItem("Do it! 안드로이드", "강XX", tempTags))
+        bookList.add(BookItem("월급 두배로 받는 법", "전XX", tempTags))
+        bookList.add(BookItem("학점 All A+ 받는 법", "성XX", tempTags))
 
         val bookAdapter = BookAdapter(this, bookList)
 
         binding.run {
             listBook.adapter = bookAdapter
-            listBook.layoutManager = LinearLayoutManager(applicationContext, RecyclerView.VERTICAL, false)
+            listBook.layoutManager =
+                LinearLayoutManager(applicationContext, RecyclerView.VERTICAL, false)
             bookAdapter.notifyDataSetChanged()
+
+            btnBookAdd.setOnClickListener {
+                startActivity(Intent(this@BookActivity, BookAddActivity::class.java))
+            }
         }
     }
 
